edc25f1662c9ac4073e744cdb2e8b3c3ebbd2bf8
Ujjawal Kumar Singh <ujjawal03.singh@gmail.com>
2025-08-06 15:37:04 +0530

    Add files via upload

---
+++ srcApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-4xl font-bold text-blue-600">SplitSpend</h1>
+      <p className="mt-4 text-gray-700">Smart Group Expense Tracker</p>
+    </div>
+  );
+};
+
+export default App;
